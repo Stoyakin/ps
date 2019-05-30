1c94d311c2f1826e59d9ae9efa7952b9c33d690d
Pavel_Stoyakin <patework@mail.ru>
2019-05-30 14:03:08 +0500

    static done

--- app/js/common.js
+++ app/js/common.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   window.ps = {};
 
-  if (qs('.js-swiper-slider')){
+  if (qs('.js-swiper-slider')) {
     window.ps.slider = new Swiper('.js-swiper-slider', {
       loop: false,
       spaceBetween: 0,
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
       },
       on: {
         slideChangeTransitionEnd: function () {
-          qs('.swiper-count-item').innerText = this.activeIndex+1;
+          qs('.swiper-count-item').innerText = this.activeIndex + 1;
         },
       },
     });
@@ -50,72 +50,76 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   window.ps.form = {
 
-    init: function init() {
-      let _th = this,
-        forms = qsAll('form'),
-        fieldPhones = qsAll('.js-phone'),
-        formElems = qsAll('.form__field-input, .form__field-textarea');
-      for (let phoneItem of fieldPhones) {
-        $(phoneItem).mask('+7(999) 999-9999')
+    init: function () {
+
+      const _th = this,
+        inputs = qsAll('.from-search__input'),
+        forms = qsAll('form');
+
+      function emptyCheck(event) {
+        event.target.value.trim() === '' ?
+          event.target.classList.remove('notempty') :
+          event.target.classList.add('notempty')
       }
-      for (let formElem of formElems) {
-        if (formElem.value != '') {
-          formElem.classList.add('no-empty')
-        } else {
-          formElem.classList.remove('no-empty')
-        }
-        formElem.addEventListener('keyup', function () {
-          if (formElem.value != '') {
-            formElem.classList.add('no-empty')
-          } else {
-            formElem.classList.remove('no-empty')
-          }
-        });
+
+      for (let item of inputs) {
+        item.addEventListener('keyup', emptyCheck)
+        item.addEventListener('blur', emptyCheck)
       }
-      for (let formItem of forms) {
-        formItem.addEventListener('submit', function (event) {
-          if (!_th.checkForm($(this))) event.preventDefault()
-        });
+
+      for (let form of forms) {
+        form.addEventListener('submit', (e) => {
+          return !_th.checkForm(form) && e.preventDefault()
+        })
       }
-      return this;
+
+      return this
     },
 
-    checkForm: function checkForm(form) {
+    checkForm: function (form) {
       let checkResult = true;
-      form.find('.warning').removeClass('warning');
-      form.find('input, textarea, select').each(function () {
-        if ($(this).data('req')) {
-          switch ($(this).data('type')) {
+      const warningElems = form.querySelectorAll('.error');
+
+      if (warningElems.length) {
+        for (let warningElem of warningElems) {
+          warningElem.classList.remove('error')
+        }
+      }
+
+      for (let elem of form.querySelectorAll('input, textarea, select')) {
+        if (elem.getAttribute('data-req')) {
+          switch (elem.getAttribute('data-type')) {
             case 'tel':
               var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-              if (!re.test($(this).val())) {
-                $(this).addClass('warning');
-                checkResult = false;
+              if (!re.test(elem.value)) {
+                elem.parentNode.classList.add('error')
+                checkResult = false
               }
               break;
             case 'email':
               var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-              if (!re.test($(this).val())) {
-                $(this).addClass('warning');
-                checkResult = false;
+              if (!re.test(elem.value)) {
+                elem.parentNode.classList.add('error')
+                checkResult = false
               }
               break;
-            case 'checkbox_personal':
-              if (!$(this).is(':checked')) {
-                $(this).parents('.checkbox').addClass('warning');
-                checkResult = false;
+            case 'file':
+              if (elem.value.trim() === '') {
+                elem.parentNode.classList.add('error')
+                checkResult = false
               }
               break;
             default:
-              if ($.trim($(this).val()) === '') {
-                $(this).addClass('warning');
-                checkResult = false;
+              if (elem.value.trim() === '') {
+                elem.classList.add('error')
+                checkResult = false
               }
               break;
           }
         }
-      });
-      return checkResult;
+      }
+
+      return checkResult
     }
 
   }.init();
@@ -170,10 +174,42 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     init: function init() {
 
+      const _self = this;
+
+      for (let item of qsAll('.js-help-next')) {
+        item.addEventListener('click', function (e) {
+          const parents = getParent(this, 'tutorial__item'),
+            nextElement = parents.nextElementSibling,
+            aside = getParent(this, 'main__aside');
+          if (nextElement) {
+            if (nextElement.classList.contains('tutorial__item--step2')){
+              aside.classList.remove('show-step1');
+              aside.classList.add('show-step2');
+            }
+            if (nextElement.classList.contains('tutorial__item--mirror')){
+              aside.classList.remove('show-step1', 'show-step2');
+              aside.classList.add('dark');
+            }
+            _self.fadeOut(parents, 300, function () {
+              _self.fadeIn(nextElement, 300, 'flex');
+            });
+          }
+          e.preventDefault();
+        });
+      }
+
+      for (let item of qsAll('.js-help-close')) {
+        item.addEventListener('click', function (e) {
+          const parents = getParent(this, 'tutorial__item');
+          _self.fadeOut(parents, 300);
+          qs('.main__aside').classList.remove('active', 'show-step1', 'show-step2', 'dark');
+          e.preventDefault();
+        });
+      }
+
       return this;
     }
 
   }.init();
 
 });
-
